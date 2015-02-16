cdef1b6b7c68b7cb8a003c4bc771dc0617be1679
chillaranand <anand21nanda@gmail.com>
2015-02-16 23:25:46 +0530

    added view to review proposals

--- junction/proposals/proposal_urls.py
+++ junction/proposals/proposal_urls.py
@@ -4,7 +4,8 @@ from __future__ import unicode_literals
 # Third Party Stuff
 from django.conf.urls import patterns, url
 
-from .views import create_proposal, delete_proposal, detail_proposal, list_proposals, update_proposal
+from .views import (create_proposal, delete_proposal, detail_proposal,
+                    list_proposals, update_proposal, review_proposal)
 
 urlpatterns = patterns(
     '',
@@ -14,4 +15,5 @@ urlpatterns = patterns(
     url(r'^update/(?P<slug>[\w-]+)/$', update_proposal, name='proposal-update'),
     url(r'^(?P<slug>[\w-]+)/$', detail_proposal, name='proposal-detail'),
     url(r'^delete/(?P<slug>[\w-]+)/$', delete_proposal, name='proposal-delete'),
+    url(r'^review/(?P<slug>[\w-]+)/$', review_proposal, name='proposal-review'),
 )
